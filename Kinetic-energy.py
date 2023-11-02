becc260645dabdbaf4e7300e9734ce931906cbc7
JASPER SHARMA <96339698+Pettyman123@users.noreply.github.com>
2023-11-02 08:35:35 +0530

    Add files via upload

---
+++ Kinetic-energy.py
@@ -0,0 +1,8 @@
+import math
+
+m = float(input("The mass of objcet(in kg):"))
+v = float(input("The constant velocity of the object during motion(in m/s):"))
+
+KE = (1/2)*m*v*v
+
+print("The Kinetic energy of the object in motion is",round(KE,2),"J")
